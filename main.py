e1836f6542207b693187fee89e73d2d1eb1b9988
MisterEblan <boy4rov.da@gmail.com>
2025-11-09 10:36:48 +0300

    feat: implemented main

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from task1_functions import function_factory
-from task2_functions import is_in_ring
+from task2_functions import (
+    is_in_ring,
+    R_1,
+    R_2,
+    GREEN,
+    RED,
+    RESET
+)
 
 def main() -> None:
     task_num = int(input("Введите номера задания (1 или 2) >>> "))
@@ -11,9 +18,23 @@ def main() -> None:
 
         print(f"x={x}")
         print(f"f={f(x)}")
+        return
 
     if task_num == 2:
-        x,
+        x, y = map(
+            float,
+            input("Введите x и y через запятую >>> ").strip().split(",")
+        )
+
+        if is_in_ring(x,y, r_1=R_1, r_2=R_2):
+            print(f"{GREEN}Точка внутри кольца.{RESET}")
+            return
+        else:
+            print(f"{RED}Точка вне кольца.{RESET}")
+            return
+    else:
+        print("Неизвестный номер задания.")
+        return
 
 if __name__ == "__main__":
     main()
--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.fixture
-def in_ring_message() -> str:
-    return "Точка находится в кольце."
-
-@pytest.fixture
-def out_of_ring_message() -> str:
-    return "Точка вне кольца."
